2a5c0df23584bc57c3825ac07d8ad05ce9c6bf0a
douglas <30963374+DouglasVinicios@users.noreply.github.com>
2020-02-17 11:44:25 -0300

    atualizacao do repositorio

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const app = express()
 const admin = require('./routes/admin')
+const path = require('path')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 // confis
+    // session
+    app.use(session({
+        secret: "algo",
+        resave: true,
+        saveUninitialized: true,
+    }))
+    app.use(flash())
+    // middleware
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash('success_msg')
+        res.locals.error_msg = req.flash('error_msg')
+        res.locals.errors_validation = req.flash('errors_validation')
+        next()
+    })
     // body parser
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
@@ -13,8 +30,11 @@ const admin = require('./routes/admin')
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
     // mongoose
-    //breve
-
+    mongoose.connect("mongodb://localhost:/blogapp", { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => { console.log('Conetou ao mongodb') })
+        .catch((err) => { console.log('error: ' + err.message) })
+    // public
+    app.use(express.static(path.join(__dirname, 'public')))
 // routes
 app.use(admin.prefix, admin.router)
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const prefix = '/admin'
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const Categoria = mongoose.model('categorias')
+const { check, validationResult } = require('express-validator')
 
 router.get('/', (req, res) => {
-    res.send('pagina do admin')
+    res.render('admin/index')
 })
 
 router.get('/posts', (req, res) => {
@@ -11,7 +15,39 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.send('pagina de categorias')
+    res.render('admin/categorias')
+})
+
+router.get('/categorias/add', (req, res) => {
+    res.render('admin/form-categorias')
+})
+
+router.post('/categorias/add', [
+    check('nome')
+        .notEmpty()
+        .withMessage('Nome é necessário'),
+    check('slug')
+        .trim().notEmpty()
+        .withMessage('Slug é necessario'),
+], (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        req.flash('errors_validation', errors.array())
+        return res.redirect('/admin/categorias/add')
+    }
+    const newCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug,
+    }
+    new Categoria(newCategoria).save()
+        .then(() => {
+            res.flash('success_msg', {msg: 'Categoria adicionada com sucess'})
+        })
+        .catch((err) => {
+            req.flash('error_msg', {msg: 'Erro ao adiciona a categoria'})
+            console.log('erro: ' + err.message)
+        })
+    res.redirect('/admin/categorias/add')
 })
 
 module.exports = { prefix, router}
